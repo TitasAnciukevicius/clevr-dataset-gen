a08d1e2e9aa8175f63b498d3277d55479fa680f6
Paul Henderson <paul@pmh47.net>
2020-01-09 19:17:58 +0100

    Adding cluster script for image generation

---
+++ image_generation/render_images.cluster.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 1
+#SBATCH --mem 16G
+#SBATCH --gres=gpu:1
+#SBATCH --time 1-00:00
+
+/nfs/scistore12/chlgrp/phenders/packages/blender-2.79b-linux-glibc219-x86_64/blender --background -noaudio --python render_images.py -- --num_images=1000 --start_idx=${SLURM_ARRAY_TASK_ID}000 --output_masks=1 --use_gpu=1
+
