13657c276c3e92254c6e62d956deb749fd9e0a09
Paul Henderson <paul@pmh47.net>
2020-01-10 14:38:20 +0100

    Changing image-rendering cluster script to use batches of 100 not 1000, to avoid blender crashes

--- image_generation/render_images.cluster.sh
+++ image_generation/render_images.cluster.sh
@@ -6,5 +6,5 @@
 #SBATCH --gres=gpu:1
 #SBATCH --time 1-00:00
 
-/nfs/scistore12/chlgrp/phenders/packages/blender-2.79b-linux-glibc219-x86_64/blender --background -noaudio --python render_images.py -- --num_images=1000 --start_idx=${SLURM_ARRAY_TASK_ID}000 --output_masks=1 --use_gpu=1
+/nfs/scistore12/chlgrp/phenders/packages/blender-2.79b-linux-glibc219-x86_64/blender --background -noaudio --python render_images.py -- --num_images=100 --start_idx=${SLURM_ARRAY_TASK_ID}00 --output_masks=1 --use_gpu=1
 
